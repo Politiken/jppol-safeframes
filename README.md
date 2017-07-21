c3a10f69b6c7d0e793eb4357ee8454164c5ee676
tegner <jesper.h.tegner@jppol.dk>
2017-07-21 13:17:54 +0200

    added documentation

--- README.md
+++ README.md
@@ -1,2 +1,180 @@
 # sharedsafeframes
 JPPol shared safeframes implementation
+
+# Table of Contents
+1. [Host side setup](#host-side)
+2. [Calling banner](#calling-banner)
+
+
+# Host side
+Include jppolhost.min.js in page
+Creates an object called jppolAdOps, with the following exposed elements
+
+* [safeframeInit](#safeframeInit)
+* [setupFinalPos](#setupFinalPos)
+
+
+# Initialize JPPol Safeframes from publisher site
+## safeframeInit
+* function
+
+The initialize function takes an object as argument, with [safeframeURL](#safeframeURL) and [adtechNetworkId](#adtechNetworkId) as mandatory settings
+
+Initialize function
+``` js
+  var sfOptions = {
+    safeframeURL: '//ebimg.dk/ux/data/safeframes/index.html',
+    adtechNetworkId: '123'
+  }
+  jppolAdOps.safeframeInit(sfOptions)
+```
+
+## Options
+
+### safeframeURL
+* Mandatory [string]
+
+Set the URL of were the safeframe HTML file is placed
+
+setup example
+```js
+  var sfOptions = {    
+    safeframeURL: '//ebimg.dk/ux/data/safeframes/index.html'
+  }
+```
+
+### adtechNetworkId
+* Mandatory [string / object]
+Set the ADTECH network id, can be passed as an object to differentiate between network ids based on [device type](#device)
+
+String setup example
+```js
+  var sfOptions = {    
+    adtechNetworkId: '123'
+  }
+```
+
+Device Object setup example
+```js
+  var sfOptions = {    
+    adtechNetworkId: {
+      desktop: '123',
+      smartphone: '123.0',
+      tablet: '123'
+    }
+  }
+```
+
+### baseBannerSrc
+* Optional [string / object]
+* default: '//adserver.adtech.de/addyn/3.0/'
+Set the base banner url, from which the final banner script src will be build, can be passed as an object to differentiate between network ids based on [device type](#device)
+
+String setup example
+```js
+  var sfOptions = {    
+    baseBannerSrc: '//adserver.adtech.de/addyn/3.0/'
+  }
+```
+
+Device Object setup example
+```js
+  var sfOptions = {    
+    adtechNetworkId: {
+      desktop: '123',
+      smartphone: '123.0',
+      tablet: '123'
+    }
+  }
+```
+
+
+### device
+* Optional [string]
+* default: 'desktop'
+Set current device type, used to differentiate adtechNetworkId and baseBannerSrc
+
+Setup example
+```js
+  var sfOptions = {    
+    device: 'desktop'
+  }
+```
+
+### messageCallback
+* Optional [function]
+Set the function which should be called when the safeframe is triggering onPosMsg
+
+An object[example shown below] is passed as argument to the callback function containing 4
+
+Argument example
+``` js
+var messageObject = {
+  'placement': 'string', // string
+  'type': 'string', // ['msg'/'error']
+  'content': 'string', //
+  'nuked': nuked
+}
+```
+
+Init setup example
+```js
+  var sfOptions = {    
+    onPosMsg: handleCallBack
+  }
+```
+
+### wallpaper
+
+#### wallpaperHandler
+* Optional [boolean]
+* default: false
+
+Should be set to true if wallpaper is an option on the page, usually only for desktop devices
+
+example
+```js
+  var sfOptions = {    
+    wallpaperHandler: true
+  }
+```
+
+#### wallpaperSelector
+* Optional [string]
+
+Can be body or regular CSS selector / ID
+
+example
+```js
+  var sfOptions = {    
+    wallpaperSelector: 'adtechWallpaper'
+  }
+```
+
+#### wallpaperPositions
+* Optional [Array / string]
+
+Name of positions of which to check for wallpaper.
+
+example
+```js
+  var sfOptions = {    
+    wallpaperPositions: ['wallpaper','monster']
+  }
+```
+### allowNuke
+* Optional [boolean]
+* default: true
+
+If for some reason you wouldn't wan't the banner to collapse if no banner is loaded
+
+example
+```js
+  var sfOptions = {    
+    allowNuke: true
+  }
+```
+
+# Calling banner
+## setupFinalPos
+* function
--- host/jppolhost.js
+++ host/jppolhost.js
@@ -1,11 +1,99 @@
-(function (jppolAdOps, ebPlacement, prebidCache) {
+'use strict'
+
+;(function (jppolAdOps, prebidCache) {
+  /**
+  * Helper: mergeObject
+  * The values in the overwriteObject will always trump the values in baseObject
+  **/
+  var mergeObject = function (baseObject, overwriteObject) {
+    try {
+      var returnObj = overwriteObject
+      if (typeof overwriteObject === 'undefined') {
+        returnObj = baseObject
+      } else {
+        for (var d in baseObject) {
+          if (typeof returnObj[d] === 'undefined') {
+            returnObj[d] = baseObject[d]
+          }
+        }
+      }
+      return returnObj
+    } catch (err) {
+      console.error('mergeObj ', err)
+    }
+  }
+
+  /**
+  * Handle key-values for banners
+  **/
+  var adtechKvAdder = function (str, obj) {
+    var split_str = str.split(';')
+    var strLength = split_str.length
+    var obj = obj || {}
+    for (var i = strLength - 1; i--;) {
+      var kv = split_str[i].replace('kv', '')
+      var kvsplit = kv.split('=')
+      var k = kvsplit[0]
+      var v = kvsplit[1]
+      obj[k] = v
+    }
+    return obj
+  }
+
+  var adtechKv = {
+    rtb: 'false' // We revert the [true/false] value to match the expected value by adtech
+  }
+
+  // <%-- criteo . key value --%>
+  if (typeof crtg_content !== 'undefined' && crtg_content !== '') {
+    adtechKv = adtechKvAdder(crtg_content, adtechKv)
+  }
+
+  // <%-- blue kai . key value --%>
+  if (typeof bk_results !== 'undefined' && typeof bk_results.campaigns !== 'undefined') {
+    adtechKv.bkcmpid = []
+    adtechKv.bkuuid = []
+    for (var i in bk_results.campaigns) {
+      adtechKv.bkcmpid.push(bk_results.campaigns[i].campaign)
+      adtechKv.bkuuid.push(bk_results.campaigns[i].bkuuid)
+    }
+
+    var metatag = document.createElement('meta')
+    metatag.name = ['WS-Custom-Targeting']
+    metatag.content = ['bkcmpid='] + adtechKv.bkcmpid.join('&') + [';bkuuid='] + adtechKv.bkuuid.join('&')
+    document.getElementsByTagName('head')[0].appendChild(metatag)
+  }
+
+  var adtechKvArr = []
+  for (var key in adtechKv) {
+    adtechKvArr.push('kv' + key + '=' + adtechKv[key])
+  }
+
+  function getKeyValues (placement) {
+    console.log('getKeyValues for runner up:', placement, 'prebidCache', prebidCache)
+    var bannerKV = []
+    if (typeof prebidCache[placement] !== 'undefined' && typeof prebidCache[placement].placementKVoldSchool !== 'undefined') {
+      console.log('getKeyValues prebidCache[placement] runner up', prebidCache[placement])
+      bannerKV = adtechKvArr.concat(prebidCache[placement].placementKVoldSchool)
+      console.log('getKeyValues bannerKV after concat', bannerKV)
+      // bannerKV = prebidCache[placement].placementKVoldSchool
+    } else {
+      bannerKV = adtechKvArr
+    }
+    var returnValue = (bannerKV.length > 0) ? bannerKV.join(';') + ';' : ''
+    return returnValue
+  }
+
+  /***********
+  * ACTUAL SAFEFRAME IMPLEMENTATION
+  ***********/
+
   /**
   * onBeforePosMsg
   * A callback function that gets fired before any cancellable action is requested to be peformed from a a SafeFrame, such as expansion, etc. Return true out of this callback function to cancel/disallow the action in question.
+  * NOTE: Seems this is never called
   **/
-  function beforePosMsg (posID) {
-    console.log('safeframe beforePosMsg', posID)
-  }
+  function beforePosMsg (posID) { }
 
   /**
   * onPosMsg
@@ -14,42 +102,43 @@
   function posMsg (posID, type, content) {
     try {
       console.log('safeframe posMsg', posID, type, content)
-      if (content === 'ebEmpty' || type === 'error') {
+      var nuked = false
+      if (content === 'nuke' && sfOptions.allowNuke) { // || type === 'error') { // TODO: we should handle errors somehow
         console.log('safeframe posMsg nuke el:', posID)
-        var eleId = posID + '_container'
-        if (posID.indexOf('follow') !== -1) {
-          eleId = 'fnFollowWrapper_' + posID
-        }
-        document.getElementById(eleId).innerHTML = '<h1>Intet banner leveret: ' + posID + '</h1>'
-        // TODO: NUKE
-        // $sf.host.nuke(posID)
-        return
+        $sf.host.nuke(posID)
+        nuked = true
       }
-      if ((posID === 'monster' || posID === 'wallpaper') && type === 'msg') {
-        console.log('safeframe posMsg', content.split('background:')[1])
-        var bgCSS = content.split('background:')[1]
-        document.getElementById('adtechWallpaper').style.background = bgCSS
+      console.log('safeframe wallpaper', (sfOptions.wallpaperHandler && typeof sfOptions.wallpaperSelector !== 'undefined'))
+      if (sfOptions.wallpaperHandler && typeof sfOptions.wallpaperSelector !== 'undefined') {
+        if (nuked === false && type === 'msg' && sfOptions.wallpaperPositionsString.indexOf(posID) !== -1) { // (posID === 'monster' || posID === 'wallpaper')) {
+          var bgCSS = content.split('styling:')[1]
+          var wpEl = null
+          if (sfOptions.wallpaperSelector === 'body') {
+            wpEl = document.body
+          } else if (sfOptions.wallpaperSelector.match(/[#.]/)) {
+            wpEl = document.querySelector(sfOptions.wallpaperSelector)
+          } else {
+            wpEl = document.getElementById(sfOptions.wallpaperSelector)
+          }
+          if (wpEl !== null) {
+            wpEl.setAttribute('style', bgCSS)
+          }
+        }
       }
 
-      if (type === 'msg' && (posID.indexOf('follow') !== -1 || posID === 'monster' || posID === 'wallpaper')) {
-        console.log('safeframe', 'do shit with follow', 'inside if', posID, ebPlacement)
-        var eleId = posID + '_container'
-        if (posID.indexOf('follow') !== -1) {
-          eleId = 'fnFollowWrapper_' + posID
-        }
-        console.log('safeframe', 'do shit with follow', eleId)
-        var regObj = {
-          'adType': posID,
-          'eleId': eleId
-        }
-        ebPlacement.skyskraper.register(regObj)
-        console.log('safeframe', 'do shit with follow', regObj)
+      var messageObject = {
+        'placement': posID,
+        'type': type,
+        'content': content,
+        'nuked': nuked
+      }
+
+      if (typeof sfOptions.messageCallback === 'function') {
+        sfOptions.messageCallback(messageObject)
       }
     } catch (err) {
       console.error('safeframe posMsg follow error', err)
     }
-
-      // console.log('safeframe posMsg status', $sf.host.status(posID))
   }
 
   /**
@@ -69,111 +158,112 @@
   }
 
   /**
-  * safeframes setup for page
-  **/
-  jppolAdOps.safeframeURL = 'http://ebimg.dk/ux/data/safeframe/safeframe.html?bring=29'
-  jppolAdOps.conf = new $sf.host.Config({
-    debug: true,
-    renderFile: jppolAdOps.safeframeURL,
-    onStartPosRender: startPosRender,
-    onEndPosRender: endPosRender,
-    onBeforePosMsg: beforePosMsg,
-    onPosMsg: posMsg
-  })
-
-  var adtechKvAdder = function (str, obj) {
-    var split_str = str.split(';'),
-      strLength = split_str.length,
-      obj = obj || {}
-    for (var i = strLength - 1; i--;) {
-      var kv = split_str[i].replace('kv', ''),
-        kvsplit = kv.split('='),
-        k = kvsplit[0],
-        v = kvsplit[1]
-      obj[k] = v
-    }
-    return obj
-  }
-
-  jppolAdOps.adtechKv = {
-    rtb: 'false' // We revert the [true/false] value to match the expected value by adtech
-  }
-
-  // <%-- criteo . key value --%>
-  if (typeof crtg_content !== 'undefined' && crtg_content !== '') {
-    jppolAdOps.adtechKv = adtechKvAdder(crtg_content, jppolAdOps.adtechKv)
-  }
-
-  // <%-- blue kai . key value --%>
-  if (typeof bk_results !== 'undefined' && typeof bk_results.campaigns !== 'undefined') {
-    jppolAdOps.adtechKv.bkcmpid = []
-    jppolAdOps.adtechKv.bkuuid = []
-    for (var i in bk_results.campaigns) {
-      jppolAdOps.adtechKv.bkcmpid.push(bk_results.campaigns[i].campaign)
-      jppolAdOps.adtechKv.bkuuid.push(bk_results.campaigns[i].bkuuid)
-    }
-
-    var metatag = document.createElement('meta')
-    metatag.name = ['WS-Custom-Targeting']
-    metatag.content = ['bkcmpid='] + jppolAdOps.adtechKv.bkcmpid.join('&') + [';bkuuid='] + jppolAdOps.adtechKv.bkuuid.join('&')
-    document.getElementsByTagName('head')[0].appendChild(metatag)
-  }
-
-  jppolAdOps.adtechKvArr = []
-  for (var key in jppolAdOps.adtechKv) {
-    jppolAdOps.adtechKvArr.push('kv' + key + '=' + jppolAdOps.adtechKv[key])
-  }
-
-  jppolAdOps.getKeyValues = function (placement) {
-    console.log('getKeyValues for runner up:', placement, 'prebidCache', prebidCache, 'adtechKv', jppolAdOps.adtechKvString)
-    var bannerKV = []
-    if (typeof prebidCache[placement] !== 'undefined' && typeof prebidCache[placement].placementKVoldSchool !== 'undefined') {
-      console.log('getKeyValues prebidCache[placement] runner up', prebidCache[placement])
-      bannerKV = jppolAdOps.adtechKvArr.concat(prebidCache[placement].placementKVoldSchool)
-      console.log('getKeyValues bannerKV after concat', bannerKV)
-      // bannerKV = prebidCache[placement].placementKVoldSchool
-    } else {
-      bannerKV = jppolAdOps.adtechKvArr
-    }
-    var returnValue = (bannerKV.length > 0) ? bannerKV.join(';') + ';' : ''
-    return returnValue
-  }
-
-  /**
   * Set up specific banner Position safeframe
   **/
   jppolAdOps.setupFinalPos = function (positionData) {
-    /**
-    * Setup data sharing
-    **/
-    var shared_data = {
-      banner_position: positionData.id,
-      banner_label: positionData.prefixit,
-      content_id: positionData.bannerID,
-      partner_id: 99
-    }
-    var private_data_key = 'sfEB'
-    var private_data = {
-      section_id: 2342,
-      site_id: 23904
-    }
-    var posMeta = new $sf.host.PosMeta(shared_data, private_data_key, private_data)
-    console.log('safeframe', 'setupFinalPos', positionData)
-    if (typeof positionData.id !== 'undefined') {
-      var keyValueString = jppolAdOps.getKeyValues(positionData.id)
-      console.log('safeframe', 'so kv for:', positionData.id, 'is', keyValueString)
-      var type = (positionData.id.indexOf('follow') !== -1) ? 1489 : 277
-      var bannerSrc = 'http://adserver.adtech.de/addyn|3.0|323|' + positionData.bannerID + '|0|' + type + '|ADTECH;loc=100;target=_blank;key=key1+key2+key3+key4;' + keyValueString + 'grp=[group];misc=' + new Date().getTime() // monster
-      console.log('safeframe', 'so bannerSrc for:', positionData.id, 'is', bannerSrc, 'with conf', positionData.posConf, 'and meta', posMeta)
-      var pos = new $sf.host.Position({
-        id: positionData.id,
-        src: bannerSrc,
-        conf: positionData.posConf,
-        meta: posMeta
-      })
-      $sf.host.render(pos)
+    try {
+      /**
+      * Setup data sharing
+      **/
+      var shared_data_defaults = {
+        banner_position: positionData.id,
+        banner_label: positionData.prefixit,
+        content_id: positionData.bannerID
+      }
+
+      var shared_data = mergeObject(shared_data_defaults, positionData.shared_data)
+
+      var private_data_key = sfDataPrivate.key || ''
+      var private_data = sfDataPrivate || {}
+
+      var posMeta = new $sf.host.PosMeta(shared_data, private_data_key, private_data)
+
+      console.log('safeframe', 'setupFinalPos', positionData)
+      if (typeof positionData.id !== 'undefined') {
+        var posConf = new $sf.host.PosConfig({
+          id:	positionData.id, // position ID
+          dest: positionData.destID, // ID of element in parent page
+          tgt: '_blank',
+          w: positionData.sfPos.sfWidth, // width of iframe
+          h: positionData.sfPos.sfHeight, // height of iframe
+          z: positionData.sfPos.zIndex
+        })
+
+        var keyValueString = getKeyValues(positionData.id)
+
+        console.log('safeframe', 'so kv for:', positionData.id, 'is', keyValueString, 'and type is', positionData.type)
+        var bannerID = positionData.bannerID
+        var aliasString = ''
+        var type = positionData.type
+        if (sfOptions.device === 'smartphone') {
+          aliasString = 'alias=' + positionData.bannerID + ';'
+          bannerID = 0
+          type = -1
+        }
+        var baseBannerSrc = (typeof sfOptions.baseBannerSrc === 'object') ? sfOptions.baseBannerSrc[sfOptions.device] : sfOptions.baseBannerSrc
+        var networkId = (typeof sfOptions.adtechNetworkId === 'object') ? sfOptions.adtechNetworkId[sfOptions.device] : sfOptions.adtechNetworkId
+        var bannerSrc = baseBannerSrc + networkId + '/' + bannerID + '/0/' + type + '/ADTECH;loc=100;' + aliasString + 'target=_blank;key=key1+key2+key3+key4;' + keyValueString + 'grp=[group];misc=' + new Date().getTime()
+        console.log('safeframe', 'so bannerSrc for:', positionData.id, 'is', bannerSrc, 'with conf', posConf, 'and meta', posMeta, positionData)
+        var pos = new $sf.host.Position({
+          id: positionData.id,
+          src: bannerSrc,
+          conf: posConf,
+          meta: posMeta
+        })
+        $sf.host.render(pos)
+      }
+    } catch (err) {
+      console.error('jppolAdOps.setupFinalPos', err)
     }
   }
 
   console.log('safeframe init $sf.host.boot()')
-}(window.jppolAdOps = window.jppolAdOps || {}, window.ebPlacement = window.ebPlacement || {}, window.prebidCache = window.prebidCache || {}))
+
+  /**
+  * Initialize jppol safeframes for publication
+  **/
+  var sfOptions
+  var sfDataPrivate = {
+    'key': 'sfOnEB'
+  }
+  var sfDefaults = {
+    'device': 'desktop',
+    'safeframeURL': '//ebimg.dk/ux/data/safeframe/safeframe.html?v=9',
+    'baseBannerSrc': {
+      'desktop': '//adserver.adtech.de/addyn/3.0/',
+      'smartphone': '//a.adtech.de/addyn/3.0/',
+      'tablet': '//adserver.adtech.de/addyn/3.0/'
+    },
+    'adtechNetworkId': {
+      'desktop': '323',
+      'smartphone': '323.0',
+      'tablet': '323'
+    },
+    'wallpaperHandler': false,
+    'wallpaperSelector': 'adtechWallpaper',
+    'wallpaperPositions': ['monster', 'wallpaper'],
+    'allowNuke': true
+  }
+
+  jppolAdOps.safeframeInit = function (options) {
+    console.log('safeframeInit', options)
+    try {
+      sfOptions = mergeObject(sfDefaults, options)
+      console.log('new safeframe', sfOptions)
+      sfOptions.wallpaperPositionsString = (typeof sfOptions.wallpaperPositions === 'string') ? sfOptions.wallpaperPositions : sfOptions.wallpaperPositions.join(',')
+      /**
+      * safeframes setup for page
+      **/
+      var jppolSafeFrameConf = new $sf.host.Config({
+        debug: true,
+        renderFile: sfOptions.safeframeURL,
+        onStartPosRender: startPosRender,
+        onEndPosRender: endPosRender,
+        onBeforePosMsg: beforePosMsg,
+        onPosMsg: posMsg
+      })
+    } catch (err) {
+      console.error('jppolAdOps.safeframeInit', err)
+    }
+  }
+}(window.jppolAdOps = window.jppolAdOps || {}, window.prebidCache = window.prebidCache || {}))
